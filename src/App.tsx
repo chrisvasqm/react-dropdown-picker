0311b8a61597e1c297a947d002504b3a0634063d
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-18 22:39:24 -0400

    Add items to shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Button, Container, HStack, Heading, Stack } from '@chakra-ui/react';
 import Dropdown from './components/Dropdown';
 import data from './data/fruits.json';
+import { useState } from 'react';
 
 function App() {
   const fruits = data.map(fruit => fruit.name);
+  const [shoppingCart, setShoppingCart] = useState<string[]>([]);
+
+  function handleSelected(item: string) {
+    setShoppingCart([...shoppingCart, item]);
+  }
 
   return (
     <Container size={['md', 'sm']} padding={2}>
@@ -11,9 +17,11 @@ function App() {
         <Heading>Dropdown Picker App</Heading>
 
         <HStack spacing={2}>
-          <Dropdown items={fruits} />
+          <Dropdown items={fruits} onSelected={handleSelected} />
           <Button colorScheme='blue'>Add</Button>
         </HStack>
+
+        <ol>{shoppingCart && shoppingCart.map(item => <li>{item}</li>)}</ol>
       </Stack>
     </Container>
   );
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -2,11 +2,12 @@ import { Select } from '@chakra-ui/react';
 
 interface Props {
   items: string[];
+  onSelected: (item: string) => void;
 }
 
-function Dropdown({ items }: Props) {
+function Dropdown({ items, onSelected }: Props) {
   return (
-    <Select placeholder='Select option'>
+    <Select placeholder='Select option' onChange={e => onSelected(e.target.value)}>
       {items &&
         items.map(item => (
           <option key={item} value={item}>
